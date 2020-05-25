6795b6b499111e6e542b28f07a491e4fbbb622be
Adrian Romański <muszynianka1337@gmail.com>
2020-05-25 22:35:31 +0200

    Added Creational - Singleton

---
+++ src/adrianromanski/creational/singleton/LazyInitialization.java
@@ -0,0 +1,17 @@
+package adrianromanski.creational.singleton;
+
+import java.util.Objects;
+
+public class LazyInitialization {
+
+    private static LazyInitialization INSTANCE = null;
+
+    private LazyInitialization() { }
+
+    public synchronized static LazyInitialization getInstance() {
+        if(Objects.isNull(INSTANCE)) {
+            INSTANCE = new LazyInitialization();
+        }
+        return INSTANCE;
+    }
+}
