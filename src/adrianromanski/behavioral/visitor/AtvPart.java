a365c10fc678ebdd86aee5fb9f746abdd8010bdd
Adrian Romański <muszynianka1337@gmail.com>
2020-05-30 18:22:03 +0200

    Added Behavioral - Visitor

---
+++ src/adrianromanski/behavioral/visitor/AtvPart.java
@@ -0,0 +1,5 @@
+package adrianromanski.behavioral.visitor;
+
+public interface AtvPart {
+	public void accept(AtvPartVisitor visitor);
+}
---
+++ src/adrianromanski/behavioral/visitor/Wheel.java
@@ -0,0 +1,8 @@
+package adrianromanski.behavioral.visitor;
+
+public class Wheel implements AtvPart {
+	@Override
+	public void accept(AtvPartVisitor visitor) {
+		visitor.visit(this);
+	}
+}
