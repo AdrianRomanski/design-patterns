dd910ae51ed67980acc7ad4b75b4890d52f720a7
adrianromanski <adrianromanski@icloud.com>
2020-07-17 14:56:12 +0200

    Added Template Method

---
+++ src/adrianromanski/head_first/template_method/barista/Coffee.java
@@ -0,0 +1,10 @@
+package adrianromanski.head_first.template_method.barista;
+
+public class Coffee extends CaffeineBeverage {
+	public void brew() {
+		System.out.println("Dripping Coffee through filter");
+	}
+	public void addCondiments() {
+		System.out.println("Adding Sugar and Milk");
+	}
+}
---
+++ src/adrianromanski/head_first/template_method/barista/Tea.java
@@ -0,0 +1,10 @@
+package adrianromanski.head_first.template_method.barista;
+
+public class Tea extends CaffeineBeverage {
+	public void brew() {
+		System.out.println("Steeping the tea");
+	}
+	public void addCondiments() {
+		System.out.println("Adding Lemon");
+	}
+}
